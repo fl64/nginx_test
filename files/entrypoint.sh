0c1c64e8275cc1dad40218105859efca124d3d1e
fl64 <flsixtyfour@gmail.com>
2020-06-30 09:26:52 +0300

    added some colors

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,7 +3,7 @@ echo -e "\n\nGenerating site.conf"
 j2 /nginx.conf.j2 -o /etc/nginx/nginx.conf
 TEST_ENV=$(env)
 TEST_RESOLVE=$(cat /etc/resolv.conf)
-TEST_NGINX_INFO="<b>Env:</b><br />$TEST_ENV<br /><b>Resolver:</b><br />${TEST_RESOLVE}<br />"
+TEST_NGINX_INFO="<b>Env:</b><br />$TEST_ENV<br /><br /><b>Resolver:</b><br />${TEST_RESOLVE}<br />"
 TEST_NGINX_INFO="${TEST_NGINX_INFO//$'\n'/<br />}"
 export TEST_NGINX_INFO
 j2 /index.html.j2 -o /var/www/html/index.html
